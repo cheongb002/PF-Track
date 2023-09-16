24a6b5f067a45a491fa32c29665cfd52f41c8ca6
Brian Cheong <cheongb002@gmail.com>
2023-09-16 05:22:48 +0000

    turn off flip aug, regen pkl

--- projects/configs/tracking/refactored/lidar/f3.py
+++ projects/configs/tracking/refactored/lidar/f3.py
@@ -2,40 +2,67 @@ _base_ = [
     './f1_q500_800x320.py',
 ]
 
+# remove flip and rot/scale/trans augmentations
+train_pipeline = [
+    dict(
+        type='mmdet3d.LoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=5,
+        use_dim=5,
+        backend_args={{_base_.backend_args}}),
+    dict(
+        type='mmdet3d.LoadPointsFromMultiSweeps',
+        sweeps_num=9,
+        load_dim=5,
+        use_dim=5,
+        pad_empty_sweeps=True,
+        remove_close=True,
+        backend_args={{_base_.backend_args}}),
+    dict(type='mmdet3d.TrackLoadAnnotations3D', 
+         with_bbox_3d=True, 
+         with_label_3d=True, 
+         with_forecasting=True),
+    dict(type='mmdet3d.PointsRangeFilter', point_cloud_range={{_base_.point_cloud_range}}),
+    dict(type='mmdet3d.TrackInstanceRangeFilter', point_cloud_range={{_base_.point_cloud_range}}),
+    dict(type='mmdet3d.TrackObjectNameFilter', classes={{_base_.class_names}}),
+    dict(type='mmdet3d.PointShuffle'),
+]
+
 # turn off cbgs to speed up training
 num_frames_per_sample = 3
 # without CBGS
-# train_dataloader = dict(
-#     dataset=dict(
-#         _delete_=True,
-#         type={{_base_.dataset_type}},
-#         num_frames_per_sample=num_frames_per_sample,
-#         forecasting=True,
-#         data_root={{_base_.data_root}},
-#         ann_file={{_base_.train_pkl_path}},
-#         pipeline={{_base_.train_pipeline}},
-#         pipeline_multiframe={{_base_.train_pipeline_multiframe}},
-#         metainfo={{_base_.metainfo}},
-#         modality={{_base_.input_modality}},
-#         test_mode=False,
-#         data_prefix={{_base_.data_prefix}},
-#         # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
-#         # and box_type_3d='Depth' in sunrgbd and scannet dataset.
-#         use_valid_flag=True,
-#         box_type_3d='LiDAR',
-#         filter_empty_gt=True,
-#         backend_args={{_base_.backend_args}}
-#     )
-# )
-
-# With CBGS
 train_dataloader = dict(
     dataset=dict(
-        dataset=dict(
-            num_frames_per_sample=num_frames_per_sample
-        )
+        _delete_=True,
+        type={{_base_.dataset_type}},
+        num_frames_per_sample=num_frames_per_sample,
+        forecasting=True,
+        data_root={{_base_.data_root}},
+        ann_file={{_base_.train_pkl_path}},
+        pipeline=train_pipeline,
+        pipeline_multiframe={{_base_.train_pipeline_multiframe}},
+        metainfo={{_base_.metainfo}},
+        modality={{_base_.input_modality}},
+        test_mode=False,
+        data_prefix={{_base_.data_prefix}},
+        # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
+        # and box_type_3d='Depth' in sunrgbd and scannet dataset.
+        use_valid_flag=True,
+        box_type_3d='LiDAR',
+        filter_empty_gt=True,
+        backend_args={{_base_.backend_args}}
     )
 )
+
+# With CBGS
+# train_dataloader = dict(
+#     dataset=dict(
+#         dataset=dict(
+#             num_frames_per_sample=num_frames_per_sample,
+#             pipeline=train_pipeline,
+#         )
+#     )
+# )
 
 model = dict(
     tracking=True,
